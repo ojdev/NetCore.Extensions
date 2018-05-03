13dcde5f88bcf341123172b1aebf9086c8d50340
俊 欧 <dev_ouj@hotmail.com>
2018-05-03 13:14:50 +0800

    初始化项目，添加String的IsNullOrWhiteSpace方法

---
+++ HC.dnc.Extensions/HC.dnc.Extensions/SystemExtensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNC.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Indicates whether a specified string is null, empty, or consists only of white-space
+        /// characters.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>true if the value parameter is null or System.String.Empty, or if value consists. exclusively of white-space characters.</returns>
+        public static bool IsNullOrWhiteSpace(this string value) => string.IsNullOrWhiteSpace(value);
+    }
+}
