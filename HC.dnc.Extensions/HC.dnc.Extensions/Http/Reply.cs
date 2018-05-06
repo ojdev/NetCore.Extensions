cf75863a34b639c60de77c07aa7fcd6d310236fe
OuJun <luacloud@gmail.com>
2018-05-06 23:25:32 +0800

    修改返回方法

--- HC.dnc.Extensions/HC.dnc.Extensions/Http/Reply.cs
+++ HC.dnc.Extensions/HC.dnc.Extensions/Http/Reply.cs
@@ -13,32 +13,40 @@ namespace DNC.Extensions.Http
         /// <summary>
         /// 是否成功
         /// </summary>
-        public bool Success { private set; get; }
+        public bool Success { private set; get; } = true;
         /// <summary>
         /// 返回值
         /// </summary>
         public Tin Result { private set; get; }
-        /// <summary>
-        /// 错误信息
-        /// </summary>
-        public Error[] ErrorInfo { private set; get; }
         /// <summary>
         /// 成功的返回内容
         /// </summary>
         /// <param name="result"></param>
         public Reply(Tin result)
         {
-            Success = true;
             Result = result;
         }
+    }
+    /// <summary>
+    /// 错误的返回内容
+    /// </summary>
+    public class ErrorReply
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { private set; get; } = false;
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        public Error[] Errors { private set; get; }
         /// <summary>
         /// 失败返回的错误信息
         /// </summary>
         /// <param name="errors"></param>
-        public Reply(Error[] errors)
+        public ErrorReply(Error[] errors)
         {
-            Success = false;
-            ErrorInfo = errors;
+            Errors = errors;
         }
     }
 }
